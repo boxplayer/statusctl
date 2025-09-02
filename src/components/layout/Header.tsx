14113518f71defbf9e65f899a2e17a3e5e1a9ac3
Jan Wadolowski <j_wadolowski@hotmail.com>
2025-09-02 15:13:49 +0200

    made logo mobile friendly, rearranged top buttons

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,19 +13,21 @@ export const asciiLogo = `
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between p-2">
-      <a
-        href="https://github.com/boxplayer/statusctl"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Button variant="outline" size="icon">
-          <Github className="h-[1.2rem] w-[1.2rem]" />
-        </Button>
-      </a>
-
+    <header className="flex items-center justify-between p-2 ">
+      <div className="flex flex-col gap-2" />
       <pre className="logo-ascii">{asciiLogo}</pre>
-      <ModeToggle />
+      <div className="flex flex-col gap-2">
+        <a
+          href="https://github.com/boxplayer/statusctl"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button variant="outline" size="icon">
+            <Github className="h-[1.2rem] w-[1.2rem]" />
+          </Button>
+        </a>
+        <ModeToggle />
+      </div>
     </header>
   );
 }
